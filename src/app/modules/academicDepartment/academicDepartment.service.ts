d98677027400e55741cb70730dee1a05acb3bc86
Abdullah Sakib <abdullahsakib888@gmail.com>
2023-09-17 15:38:25 +0600

    Completed half of the API gateway

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,65 @@
+import { Request } from 'express';
+import { IGenericResponse } from '../../../interfaces/common';
+import { CoreService as HttpService } from '../../../shared/axios';
+
+const insertIntoDB = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.post(`/academic-department`, req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const getAllFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.get('/academic-department', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const getByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.get(`/academic-department/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+const updateOneInDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  console.log('l;asdfjosdfjs;lfkjsal;df ');
+  const response: IGenericResponse = await HttpService.patch(
+    `/academic-department/${id}`,
+    req.body,
+    {
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    }
+  );
+  return response;
+};
+
+const deleteByIdFromDB = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.delete(`/academic-department/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
+
+export const AcademicDepartmentService = {
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneInDB,
+  deleteByIdFromDB,
+  insertIntoDB
+};
---
+++ src/app/routes/index.ts
@@ -0,0 +1,76 @@
+import express from 'express';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.router';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.router';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.router';
+import { buildingRoutes } from '../modules/building/building.router';
+import { courseRoutes } from '../modules/course/course.router';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.router';
+import { offeredCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.router';
+import { roomRoutes } from '../modules/room/room.router';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.router';
+import { studentEnrolledCourseRoutes } from '../modules/studentEnrolledCourse/studentEnrolledCourse.router';
+import { offeredCourseClassScheduleRoutes } from '../modules/offeredCourseClassSchedule/offeredCourseClassSchedule.router';
+import { studentEnrolledCourseMarkRoutes } from '../modules/studentEnrolledCourseMark/studentEnrolledCourseMark.router';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/academic-semesters',
+    routes: AcademicSemesterRoutes
+  },
+  {
+    path: '/academic-departments',
+    routes: AcademicDepartmentRoutes
+  },
+  {
+    path: '/academic-faculties',
+    routes: AcademicFacultyRoutes
+  },
+  {
+    path: '/buildings',
+    routes: buildingRoutes
+  },
+  {
+    path: '/rooms',
+    routes: roomRoutes
+  },
+  {
+    path: '/courses',
+    routes: courseRoutes
+  },
+  {
+    path: '/semester-registrations',
+    routes: semesterRegistrationRoutes
+  },
+  {
+    path: '/offered-courses',
+    routes: offeredCourseRoutes
+  },
+  {
+    path: '/offered-course-sections',
+    routes: offeredCourseSectionRoutes
+  },
+  {
+    path: '/offered-course-class-schedules',
+    routes: offeredCourseClassScheduleRoutes
+  },
+  {
+    path: '/student-enrolled-courses',
+    routes: studentEnrolledCourseRoutes
+  },
+  {
+    path: '/student-enrolled-course-marks',
+    routes: studentEnrolledCourseMarkRoutes
+  }
+  // {
+  //   path: '/student-semester-payments',
+  //   routes: studentSemesterPaymentRoutes
+  // }
+];
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.routes);
+});
+
+export default router;
